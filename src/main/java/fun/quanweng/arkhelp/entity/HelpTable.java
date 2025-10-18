d0d9c636527a18f18628f32ec87c95904459fdaa
QuanWenG <602901318@qq.com>
2025-10-19 04:22:34 +0800

    arkHelp项目开工和实体类构建

--- src/main/java/fun/quanweng/arkhelp/entity/HelpTable.java
+++ src/main/java/fun/quanweng/arkhelp/entity/HelpTable.java
@@ -15,18 +15,22 @@ public class HelpTable {
     * 逻辑id
     * */
     private Long id;
+
     /**
     * 创建者id
     * */
     private Long masterId;
+
     /**
      * 渠道来源 0:官服 1:B服 2:国际服 3:台服
      */
     private Integer tableFrom;
+
     /**
      * 创建时间
      */
     private LocalDateTime createTime;
+
     /**
      * 最近更改时间
      */
--- src/main/java/fun/quanweng/arkhelp/entity/UserTable.java
+++ src/main/java/fun/quanweng/arkhelp/entity/UserTable.java
@@ -15,22 +15,27 @@ public class UserTable {
      * 逻辑id
      */
     private Long id;
+
     /**
      * 账号
      */
     private String userName;
+
     /**
      * 密码
      */
     private String password;
+
     /**
      * 账号状态
      */
     private Character status;
+
     /**
      * 创建时间
      */
     private LocalDateTime createTime;
+
     /**
      * 更新时间
      */
